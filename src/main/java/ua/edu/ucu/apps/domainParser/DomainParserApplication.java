b43caae8107f36bf810932aa5281d22246959cc8
LiubMok <liubik04@gmail.com>
2022-11-30 14:09:00 +0200

    Create project

---
+++ src/main/java/ua/edu/ucu/apps/domainParser/DomainParserApplication.java
@@ -0,0 +1,13 @@
+package ua.edu.ucu.apps.domainParser;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DomainParserApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DomainParserApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ua/edu/ucu/apps/domainParser/DomainParserApplicationTests.java
@@ -0,0 +1,13 @@
+package ua.edu.ucu.apps.domainParser;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DomainParserApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
