7f250992b4316b13aecb8c45481ee182b8bd4882
Admin <mkruhliy@gmail.com>
2022-12-03 15:07:46 +0200

    Web merged

---
+++ src/main/java/ua/edu/ucu/apps/domainParser/Controllers/HomeController.java
@@ -0,0 +1,51 @@
+package ua.edu.ucu.apps.domainParser.Controllers;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+import ua.edu.ucu.apps.domainParser.Classes.DomainData;
+import ua.edu.ucu.apps.domainParser.Classes.DomainInput;
+import ua.edu.ucu.apps.domainParser.Classes.SearchEngine;
+import ua.edu.ucu.apps.domainParser.Services.DataService;
+
+import java.util.List;
+
+@Controller
+@RequestMapping("/api")
+public class HomeController {
+
+    private final DataService dataService;
+    private final SearchEngine searchEngine;
+
+    public HomeController(@Autowired DataService database, @Autowired SearchEngine searchEngine) {
+        this.dataService = database;
+        this.searchEngine = searchEngine;
+    }
+
+    @GetMapping("/")
+    public String welcome() {
+        return "index.html";
+    }
+
+    @GetMapping("/find")
+    @ResponseBody
+//    List<DomainData>
+    public List<DomainData> getData(@RequestParam String domainString) {
+        System.out.println(domainString);
+        DomainInput inputDomain = new DomainInput(domainString);
+        Logger logger = LoggerFactory.getLogger(DataController.class);
+        DomainData domain = dataService.findOneByDomain(inputDomain.getName());
+        if (domain == null){
+            domain = searchEngine.searchInfoAboutCompany(inputDomain.getName());
+            domain.changeNull();
+            dataService.save(domain);
+        }
+
+        List<DomainData> allDomains = dataService.getAllDomains();
+        System.out.println(allDomains);
+        return allDomains;
+    }
+
+}
